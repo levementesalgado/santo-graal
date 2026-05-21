75cc6e07583660923c56146ce501dfe548cff1c5
levementesalgado <mateusvideos49@gmail.com>
2026-05-21 10:17:12 -0300

    feat(api/demo): demo.ts - dados fictícios com tipos reais, ou: como enganar o TypeScript

--- src/api/demo.ts
+++ src/api/demo.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-export const getUser = async (id) => {
+export const getUser = async (id: string): Promise<unknown> => {
   const response = await axios.get(`https://api.example.com/api/users/${id}`);
   return response.data;
 };
 
-export const createUser = async (data) => {
+export const createUser = async (data: unknown): Promise<unknown> => {
   const response = await axios.post("https://api.example.com/api/users", data);
   return response.data;
 };
